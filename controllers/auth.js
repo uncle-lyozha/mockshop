7283cf092cdada0931283e34d007e8805e6e3233
Uncle Lyozha <jee.rastafar@gmail.com>
2023-11-24 14:22:45 +0300

    add session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,32 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
+  console.log(req.session);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login.",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.redirect('/')
+  User.findById("655e2f168e61c4e65302d9de")
+    .then(user => {
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
