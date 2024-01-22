84d8efec5c01def72a0fdc34a803508e47bc0b46
Uncle Lyozha <jee.rastafar@gmail.com>
2024-01-22 21:57:42 +0300

    add pagination,js pub, delete products via rest req

--- controllers/admin.js
+++ controllers/admin.js
@@ -209,8 +209,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -221,11 +221,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(product => {
       console.log(`Product Destroyed: \n ${product}`);
-      res.redirect("/admin/products");
+      res.status(200).json({message: "Success!"});
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message: "Deleteing failed."});
     });
 };
